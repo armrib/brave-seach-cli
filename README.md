99c6293571298c36aecd4c2654c3c7142ca9222b
Armand Ribouillault <armand.ribouillault@gmail.com>
2024-02-02 12:56:09 +0100

    Add script

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# brave-seach-cli
+# Brave seach cli
 A CLI to access brave search result browserless !
+I use w3m to browse in the command line.
+If Codellm or Summarize is not available, the script fall back to w3m browsing.
+
+## To use it
+You can put the function in your .bashrc or load it from there.
+
+## Side notes
+To use CodeLLM, a search has to be done before.
+Summarize works the same way.
+
+## Improvements
+You can create issues or pull request directly to improve it.
---
+++ brave_search_cli.sh
@@ -0,0 +1,9 @@
+# Usage example: search how to extract a field from an object in an array with jq
+# Packages used: curl, sed, jq, w3m
+function search() { 
+	(($#)) && \
+    s=$(curl "https://search.brave.com/search?q=$(echo $* | sed -e 's/ /+/g')&source=web" 2> /dev/null) && (\
+    ((echo $s  | w3m -T text/html -dump | grep -q '^Code LLM') && curl "https://search.brave.com/api/codellm?key=codellm:$(echo $* | sed -e 's/ /%20/g')%3Aus%3Aen" && echo) \
+    || ((echo $s  | w3m -T text/html -dump | grep -q '^Summarizer') && curl "https://search.brave.com/api/summarizer?key=$(echo $* | sed -e 's/ /%20/g')%3Aus%3Aen" 2> /dev/null | jq -r '.results[].text' | w3m -T text/html -dump && echo) \
+    ) || echo $s | w3m -T text/html -o confirm_qq=false;
+}
