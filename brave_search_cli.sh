39b89c61182970c8ca68392d222da1281abffc41
Armand Ribouillault <armand.ribouillault@gmail.com>
2024-02-08 18:10:40 +0100

    small fix

--- brave_search_cli.sh
+++ brave_search_cli.sh
@@ -5,7 +5,7 @@
 function search() { 
 	(($#)) && \
     s=$(curl "https://search.brave.com/search?q=$(echo $* | sed -e 's/ /+/g')&source=web" 2> /dev/null) && (\
-    ((echo $s  | w3m -T text/html -dump | grep -q '^Code LLM') && curl "https://search.brave.com/api/codellm?key=codellm:$(echo $* | sed -e 's/ /%20/g')%3Aus%3Aen" && echo) \
+    ((echo $s  | w3m -T text/html -dump | grep -q '^Code LLM') && curl "https://search.brave.com/api/codellm?key=codellm%3A$(echo $* | sed -e 's/ /%20/g')%3Aus%3Aen" && echo) \
     || ((echo $s  | w3m -T text/html -dump | grep -q '^Summarizer') && curl "https://search.brave.com/api/summarizer?key=$(echo $* | sed -e 's/ /%20/g')%3Aus%3Aen" 2> /dev/null | jq -r '.results[].text' | w3m -T text/html -dump && echo) \
     ) || echo $s | w3m -T text/html -o confirm_qq=false;
 }
